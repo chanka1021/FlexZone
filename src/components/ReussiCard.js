bf8649a8fa728839f845e1ceafa75984c9f13921
Ashraf <59064232+chanka1021@users.noreply.github.com>
2023-12-09 23:12:13 +0100

    reussCard

--- src/components/ReussiCard.js
+++ src/components/ReussiCard.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import img from '../assets/reussiPhoto.png'
+import  './styles/ReussiteCard.css'
 
 function ReussiCard() {
   return (
     <div className='ReussiCardContainer'>
-        
+      <img src={img} />
+      <div className='reussInfos'>
+        <div className='RInfos1'>
+          <p>Madison Froning</p>
+          <p>⭐/5</p>
+        </div>
+        <div className='Rinfos 2'>
+        <p>Specialisations :</p> 
+        <p> Crossfit Expoort, Nutrition & Rehab</p>
+        <div className='Social'></div>
+        </div>
+
+      </div>
     </div>
   )
 }

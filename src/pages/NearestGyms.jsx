efa8df3849419f02863803b6ac67ad69c99650a0
FUJITSU <FUJITSU@DESKTOP-IV0DDR2>
2023-12-19 21:45:34 +0100

    the map is rendered with success

--- src/pages/NearestGyms.jsx
+++ src/pages/NearestGyms.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-
+"use client";
+import React from "react";
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
 function NearestGyms() {
+    const position  ={lat:53.54,lng:10}
   return (
-    <div>
-       
-    </div>
-  )
+    <APIProvider apiKey={process.env.REACT_APP_GOOGLE_API_KEY}>
+      <div style={{height:"100vh", width:"100%"}}>
+        <Map zoom={9} center={position}>
+
+        </Map>
+      </div>
+    </APIProvider>
+  );
 }
 
-export default NearestGyms 
+export default NearestGyms;

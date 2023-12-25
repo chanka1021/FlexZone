34ac33bf9a9e51163e57a82edbe87dc51b6da9d9
Ashraf <59064232+chanka1021@users.noreply.github.com>
2023-12-25 14:23:56 +0100

    dashboard -2

--- src/components/userdashboard components/UserOverView.js
+++ src/components/userdashboard components/UserOverView.js
@@ -1,11 +1,89 @@
 import React from 'react'
+import '../styles/UserOverView.css'
+import ReactECharts from 'echarts-for-react';
+
+
 function UserOverView() {
-  
+  const data = [
+    { day: 'Mon', value: 10 },
+    { day: 'Tue', value: 52 },
+    { day: 'Wed', value: 200 },
+    { day: 'Thu', value: 334 },
+    { day: 'Fri', value: 390 },
+    { day: 'Sat', value: 330 },
+    { day: 'Sun', value: 220 },
+    { day: 'Mon', value: 10 },
+    { day: 'Tue', value: 52 },
+    { day: 'Wed', value: 200 },
+    { day: 'Thu', value: 334 },
+    { day: 'Fri', value: 390 },
+    { day: 'Sat', value: 330 },
+    { day: 'Sun', value: 220 },
+    { day: 'Mon', value: 10 },
+    { day: 'Tue', value: 52 },
+    { day: 'Wed', value: 200 },
+    { day: 'Thu', value: 334 },
+    { day: 'Fri', value: 390 },
+    { day: 'Sat', value: 330 },
+    { day: 'Sun', value: 220 },
+  ];
+
+  const opts = {
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow',
+      },
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true,
+    },
+    xAxis: [
+      {
+        type: 'category',
+        data: data.map(item => item.day),
+        axisTick: {
+          alignWithLabel: true,
+        },
+      },
+    ],
+    yAxis: [
+      {
+        type: 'value',
+      },
+    ],
+    color: "#A1F65E",
+    series: [
+      {
+        name: 'Direct',
+        type: 'bar',
+        barWidth: '60%',
+        data: data.map(item => item.value),
+      },
+    ],
+  };
+
   return (
-    <div>
-      User Over View
+    <div className='UserOverViewContainer'>
+      <p>fréquentation de ce mois</p>
+      <div className='attendenceChart'>
+        <ReactECharts option={opts} />
+      </div>
+      <p>OverView </p>
+
+      <div className='userOvCardsContainer'>
+        <div className='OvCard'>
+        </div>
+        <div className='OvCard'>
+        </div><div className='OvCard'>
+        </div><div className='OvCard'>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default UserOverView
+export default UserOverView;

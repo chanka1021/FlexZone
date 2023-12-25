1d8f94187c9a5b281621da4319257a9757ea91ac
FUJITSU <FUJITSU@DESKTOP-IV0DDR2>
2023-12-25 17:49:53 +0100

    remove some default text

--- src/components/ClubInfos.js
+++ src/components/ClubInfos.js
@@ -8,7 +8,7 @@ function ClubInfos(props) {
       <p style={{textAlign:"justify"}}><a>{props.club.tel}</a> {props.club.description}</p>
       <div className='Surface-horaires-adress'>
         <div>Surface <a>{props.club.surface} m²</a></div>
-        <div>Capacité git <a>{props.club.capacity} personnes</a></div>
+        <div>Capacité <a>{props.club.capacity} personnes</a></div>
 
       </div>
     </div>

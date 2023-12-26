f489dc3e1a04ec711fbdb31ea2e4775bb3d800d2
FUJITSU <FUJITSU@DESKTOP-IV0DDR2>
2023-12-27 00:59:23 +0100

    formating text alignement

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,56 +1,76 @@
-import React, { useState ,useContext} from 'react';
-import '../components/styles/Sign.css'
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import { UserContext } from '../Util/userContext';
-
+import React, { useState, useContext } from "react";
+import "../components/styles/Sign.css";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { UserContext } from "../Util/userContext";
 
 function SignIn() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-   const {user,setUser}= useContext(UserContext)
-    const handleSignIn = async (e) => {
-        e.preventDefault();
-        // Add your sign-in logic here, e.g., make an API call to authenticate the user
-        try {
-            const data = await  axios.post("/login",{email,password})
-            console.log(data.message) 
-        } catch (error) {
-            //console.log(error)
-        }
-      
-       // console.log('Signing in with:', email, password);
-    };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { token, setToken } = useContext(UserContext);
+  const navigate = useNavigate()
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+    // Add your sign-in logic here, e.g., make an API call to authenticate the user
+    try {
+      axios
+        .post("/login", { email, password })
+        .then((res) => {
+          if (res.status === 200) {
+            //console.log(200,res)
+            setToken(res.data.data);
+            alert(res.data.message);
+            navigate("/dashboard")
+          } else {
+            alert("something wrong happends");
+          }
+        })
+        .catch((err) => {
+          alert("something wrong happends , please try again later");
+        });
 
-    return (
-        <div className='LoginForm'>
-            <h2>Se connecter</h2>
-            <form   onSubmit={handleSignIn}>
-                <label>
-                    <input
-                        placeholder='Email'
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
-                </label>
-                <br />
-                <label>
-                    <input
-                        placeholder='Mot de pass'
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </label>
-                <br />
-                <button type="submit">Sign In</button>
-            </form>
-           <p>Vous n'avez pas de compte ? <Link className='SignupLabel' to={'/signup'}>S'inscrire maintenant</Link></p> 
-        </div>
-    );
+      // console.log(data.message)
+    } catch (error) {
+      //console.log(error)
+    }
+
+    // console.log('Signing in with:', email, password);
+  };
+
+  return (
+    <div className="LoginForm">
+      <h2>Se connecter</h2>
+      <form onSubmit={handleSignIn}>
+        <label>
+          <input
+            placeholder="Email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          <input
+            placeholder="Mot de pass"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <button type="submit">Sign In</button>
+      </form>
+      <p>
+        Vous n'avez pas de compte ?{" "}
+        <Link className="SignupLabel" to={"/signup"}>
+          S'inscrire maintenant
+        </Link>
+      </p>
+    </div>
+  );
 }
 
 export default SignIn;

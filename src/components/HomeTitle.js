ce94cbe497efe9ce37ddcea342b7a3afb0cb7612
FUJITSU <FUJITSU@DESKTOP-IV0DDR2>
2023-12-27 01:24:26 +0100

    adding the link to about page

--- src/components/HomeTitle.js
+++ src/components/HomeTitle.js
@@ -1,17 +1,29 @@
-import React from 'react'
-import './styles/SectionTitle.css'
+import React from "react";
+import "./styles/SectionTitle.css";
+import { Link } from "react-router-dom";
 
 function HomeTitle(props) {
-    return (
-        <div className='HomeSectionTitle' style={{ backgroundImage: `url(${props.img})` }}>
-            <p className='SectionTitle'>Faites briller votre salle de sport <br/> sur la carte</p>
-            <div className='btns'>
-            <div className='MainBtn'>Commencer</div>
-            <span/>
-            <div className='SecBtn'>A proppos</div>
-            </div>
-        </div>
-    )
+  return (
+    <div
+      className="HomeSectionTitle"
+      style={{ backgroundImage: `url(${props.img})` }}
+    >
+      <p className="SectionTitle">
+        Faites briller votre salle de sport <br /> sur la carte
+      </p>
+      <div className="btns">
+        <Link to={"/clubs"}>
+          {" "}
+          <div className="MainBtn">Commencer</div>
+        </Link>
+        <span />
+        <Link to={"/about"}>
+          {" "}
+          <div className="SecBtn">A proppos</div>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default HomeTitle
+export default HomeTitle;

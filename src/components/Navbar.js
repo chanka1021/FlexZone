918e748c34bd722f65d892271df291c7546a3539
FUJITSU <FUJITSU@DESKTOP-IV0DDR2>
2023-12-27 18:38:14 +0100

    added the logout button that is displayed when isLoggedIn state is true

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import "./styles/Navbar.css";
 import { Link, NavLink } from "react-router-dom";
 import { MdOutlineMenu } from "react-icons/md";
 import { CgGym } from "react-icons/cg";
 import { FaRegUserCircle } from "react-icons/fa";
-import ico from "../assets/Icon.png"
+import { LuLogOut } from "react-icons/lu";
+import ico from "../assets/Icon.png";
+import { UserContext } from "../Util/userContext";
+import { useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+  const navigate = useNavigate();
   const [menuOpen, setMenuOpen] = useState(false);
-  const [isLogged, setisLogged] = useState(false);
-
-
+  const { isLoggedIn, setIsLoggedIn } = useContext(UserContext);
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+    setIsLoggedIn(false);
+  };
   return (
     <nav>
       <Link to="/" className="title">
-          <img src={ico} />
+        <img src={ico} />
         Flexzone
       </Link>
       <div className="MenuMobile" onClick={() => setMenuOpen(!menuOpen)}>
@@ -39,15 +46,23 @@ export const Navbar = () => {
         <li>
           <NavLink to="/contact">Contact</NavLink>
         </li>
-        <li  onClick={() => setisLogged(!isLogged)}>
-          {isLogged ? <div className="logged">
-            <MdOutlineMenu />
-            <FaRegUserCircle />
-          </div> :
+        <li>
+          {isLoggedIn ? (
+            <NavLink to="/dashboard">Dashboard</NavLink>
+          ) : (
             <div className="AccountBtn">
-              <NavLink to="">join club</NavLink>
+              <NavLink to="/signin">S'inscrire</NavLink>
             </div>
-          }
+          )}
+        </li>
+        <li>
+          {isLoggedIn && (
+            <div className="AccountBtn">
+            <NavLink to={"/"} className="logout-btn" onClick={handleLogout}>
+              <LuLogOut />
+            </NavLink>
+            </div>
+          )}
         </li>
       </ul>
     </nav>

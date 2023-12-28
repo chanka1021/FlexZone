248c4a61fe93f489c9eccc63ab3ba73f55dd4c51
FUJITSU <FUJITSU@DESKTOP-IV0DDR2>
2023-12-28 11:52:08 +0100

    updating yhe dashborad,persisting the user data into the localstorage and using it as a global state

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,7 +5,7 @@ import { IoCalendarOutline } from "react-icons/io5";
 import { MdOutlineProductionQuantityLimits } from "react-icons/md";
 import { GiGymBag } from "react-icons/gi";
 import { MdOutlinePriceCheck } from "react-icons/md";
-import { FaUsers } from "react-icons/fa";
+import { FaUsers, FaCamera } from "react-icons/fa";
 import { FaCartPlus } from "react-icons/fa6";
 import { BsFillNutFill, BsInfoSquare } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
@@ -14,13 +14,14 @@ import UserOverView from "../components/userdashboard components/UserOverView";
 import UsersGym from "../components/userdashboard components/UsersGym";
 import { UserContext } from "../Util/userContext";
 import axios from "axios";
+
 function Dashboard() {
   const [selectedItem, setSelectedItem] = useState(null);
   const [selectedComponent, setSelectedComponent] = useState(null);
-  const [user, setUser] = useState(null);
+  
   const navigate = useNavigate();
   //shared state
-  const { token } = useContext(UserContext);
+  const { token ,user,setUser} = useContext(UserContext);
 
   useEffect(() => {
     if (token == null) {
@@ -29,7 +30,7 @@ function Dashboard() {
     const config = {
       headers: { Authorization: `Bearer ${token}` },
     };
-   
+
     try {
       axios
         .get("/user/profile", config)
@@ -37,7 +38,7 @@ function Dashboard() {
           //console.log(res.status)
           if (res.status === 200) {
             setUser(res.data.data);
-            localStorage.setItem('user',res.data.data)
+            localStorage.setItem("user", res.data.data);
             console.log(res.data.data);
           } else {
             return navigate("/signin");
@@ -49,7 +50,7 @@ function Dashboard() {
     } catch (error) {
       // console.log(error)
     }
-  }, [token]);
+  }, [token,user]);
   const handleItemClick = (item) => {
     const selectedItemObj =
       UserNavItems.find((navItem) => navItem.id === item) ||
@@ -91,6 +92,46 @@ function Dashboard() {
   var IsGymOwner = false;
   IsGymOwner ? (NavItems = GymOwnerNavItems) : (NavItems = UserNavItems);
 
+  const handleChangeProfilePicture = () => {
+    // Trigger the click event of the hidden file input
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleFileInputChange = (event) => {
+    // Handle the selected directory or file here
+    const selectedFiles = event.target.files;
+
+    // Check if any files are selected
+    if (selectedFiles.length > 0) {
+      const picture = selectedFiles[0];
+      console.log("Selected File:", picture);
+
+      // Create a FormData object and append the file to it
+      const formData = new FormData();
+      formData.append("picture", picture);
+      try {
+        axios.post("user/set-profile-picture", formData, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "multipart/form-data", // Important for file uploads
+          },
+        }).then(res=>{
+          if(res.status===202){
+            user.picture=res.data.data
+            setUser(user)
+            localStorage.setItem('user',user)
+          }
+        })
+      } catch (error) {}
+
+      // You can perform additional logic with the selected file here
+    }
+  };
+
+  // Ref for the file input element
+  const fileInputRef = React.createRef();
   if (token && user) {
     return (
       <div className="Dashboard">
@@ -111,15 +152,42 @@ function Dashboard() {
         <div className="Actions">{selectedComponent}</div>
         <div className="UserData">
           <div className="UserDetails">
-            <img
-              className="UserImg"
-              src={"http://localhost:8000/images/profileImgs/" + user.picture}
-            />
-            <div className="UserType">{user.is_admin?"Admin":user.is_gym_owner?"Gym Owner":"User"}</div>
+            <>
+              <img
+                className="UserImg"
+                src={"http://localhost:8000/images/profileImgs/" + user.picture}
+              />
+              <FaCamera
+                className="change-profile-icon"
+                onClick={handleChangeProfilePicture}
+              ></FaCamera>
+              <input
+                type="file"
+                style={{ display: "none" }}
+                accept="image/*"
+                multiple={false}
+                onChange={handleFileInputChange}
+                ref={fileInputRef}
+              />
+            </>
+            <div className="UserType">
+              {user.is_admin
+                ? "Admin"
+                : user.is_gym_owner
+                ? "Gym Owner"
+                : "User"}
+            </div>
             <div className="UserName">
               {user.first_name + " " + user.last_name}{" "}
             </div>
-            <div className="UserJD">Membre depuis : {new Date(user.created_at).getFullYear()+"/"+new Date(user.created_at).getMonth()+"/"+new Date(user.created_at).getDate()}</div>
+            <div className="UserJD">
+              Membre depuis :{" "}
+              {new Date(user.created_at).getFullYear() +
+                "/" +
+                new Date(user.created_at).getMonth() +
+                "/" +
+                new Date(user.created_at).getDate()}
+            </div>
           </div>
           <div className="UserPersonalInfos">
             <p>détails personnels</p>

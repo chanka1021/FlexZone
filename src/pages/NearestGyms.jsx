f3130f15c34f28834d4c8bcaddb27505aa5cc55c
FUJITSU <FUJITSU@DESKTOP-IV0DDR2>
2024-01-02 01:51:51 +0100

    it works just a matter of dii99a

--- src/pages/NearestGyms.jsx
+++ src/pages/NearestGyms.jsx
@@ -25,13 +25,17 @@ function NearestGyms() {
 
   const getNearestGyms = async (lat, lng) => {
     try {
-      const response = await axios.post("http://127.0.0.1:8000/api/getNearestGymsLoaction", {
+      const response = await axios.post("/getNearestGymsLoaction", {
         lat,
         lng,
       });
-
-      setNearestGyms(response.data.gyms);
+      if(response.status===200){
+        setNearestGyms(response.data.gyms);
       console.log(nearestGyms)
+      }else{
+        alert("something wrong happends")
+      }
+      
     } catch (err) {
       console.log(err);
     }
@@ -48,7 +52,8 @@ function NearestGyms() {
   useEffect(() => {
     const fetchGyms = async ()=>{
       if (location) {
-      await getNearestGyms(location.lat, location.lng);
+        //await getNearestGyms(location.lat, location.lng);
+        await getNearestGyms(35.6175794, -5.2727389);
     }
     }
     fetchGyms()
@@ -58,7 +63,30 @@ function NearestGyms() {
 
     
  
-      
+  // function getNearbyGyms(lat,lng) {
+  //   // Replace process.env.YOUR_VARIABLE with your actual environment variable names
+   
+  //   const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
+  
+  //   // Make sure all required environment variables are set
+  //   if (!lat || !lng || !apiKey) {
+  //     console.error('Missing required environment variables');
+  //     return;
+  //   }
+  
+  //   const apiUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=gym&location=${lat}%2C${lng}&radius=15000&type=gym&key=${apiKey}`;
+  
+  //   axios.get(apiUrl)
+  //     .then(response => {
+  //       // Handle the response data here
+  //       console.log(response.data);
+  //     })
+  //     .catch(error => {
+  //       // Handle errors here
+  //       console.error('Error fetching nearby gyms:', error.message);
+  //     });
+  // }
+  
 
 
   const position = { lat: 53.54, lng: 10 };
